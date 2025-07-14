dc3c2ec1e75522b8ec51262b3269a4a4001d93f6
iamraunakmishra <129874527+iamraunakmishra@users.noreply.github.com>
2025-07-14 10:38:52 +0530

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -10,17 +10,16 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    "http://localhost:5173", 
+    "https://fullproject-khaki.vercel.app", 
+    "http://localhost:3000" 
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,        
-    allow_credentials=True,       
-    allow_methods=["*"],          
-    allow_headers=["*"],          
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 
